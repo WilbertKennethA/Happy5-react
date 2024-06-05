14114786d93fffa995770ed9172796043c4dea53
Wilbert Kenneth <wilbertkennetha@gmail.com>
2024-06-05 15:44:18 +0700

    Create sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <aside className="sidebar">
+            <img src= "src/assets/happy5.png" alt="Logo" className="logo" />
+        </aside>
+    );
+};
+
+export default Sidebar;
