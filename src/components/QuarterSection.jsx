62a757dbade6f92f0978edb3eb843098bacb2761
Wilbert Kenneth <wilbertkennetha@gmail.com>
2024-06-06 20:09:54 +0700

    final commit

--- src/components/QuarterSection.jsx
+++ src/components/QuarterSection.jsx
@@ -4,15 +4,17 @@ import TaskCard from './TaskCard';
 const QuarterSection = ({ period, month, tasks }) => {
     return (
         <div className="quarter-section">
-            <div className='period'>
-                <h2>{period}</h2>
-                <h3>{month}</h3>
-                {tasks.length === 0 ? <p>No Task Available</p> : tasks.map((task, index) => <TaskCard key={index} title={task.title} progress={task.progress}/>)}
+            <div className='content'>
+                <div className='period'>
+                    <h2>{period}</h2>
+                    <h3>{month}</h3>
+                    {tasks.length === 0 ? <p>No Task Available</p> : tasks.map((task, index) => <TaskCard key={index} title={task.title} progress={task.progress}/>)}
+                </div>
+                <button className='button'>
+                    <img src="src/assets/plus.png" alt="plus" className="plus"/>
+                    <h5>Create new task</h5>
+                </button>
             </div>
-            <button className='button'>
-                <img src="src/assets/plus.png" alt="plus" className="plus"/>
-                <h5>Create new task</h5>
-            </button>
         </div>
     );
 };
